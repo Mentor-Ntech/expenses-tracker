203046b285a310384f93828acf6bf199ff22cb32
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-02-19 18:21:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from "@material-ui/core"
+import Details from './components/Details/Details';
+import useStyles from './styles'
 
-const Hello = () => {
+
+const App = () => {
+    const classes = useStyles()
   return (
-    <div>Hello</div>
+    <div>
+        <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{height: "100vh"}}>
+            <Grid item xs={12} sm={4}>
+                <Details title="Income"/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                Main
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Details title="Expense"/>
+            </Grid>
+        </Grid>
+    </div>
   )
 }
 
-export default Hello
+export default App
